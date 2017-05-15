d9956ce5d11bfb59f4a5c8ec521693682ec9755c
suditibansal <suditi.bansal@yahoo.in>
2017-05-15 21:37:42 +0530

    app

---
+++ DemoWebApp/DemoWebApp/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoWebApp.Controllers
+{
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:32731/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync("api/Values").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsStringAsync().Result;
+                ViewBag.Message = result;
+            }
+            else
+            {
+                ViewBag.Message = "Could not connet to API";
+            }
+            
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
